d0d35e3706b0c8fa96f927dbdaf22414e6aa0fd0
itsbasil <itsbasilgeorge@gmail.com>
2024-06-19 23:50:14 +0530

    models edited

--- myenv/shop_project/products/models.py
+++ myenv/shop_project/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# Model for Product
+class Product(models.Model):
+    title=models.CharField(max_length=200)
+    price=models.FloatField()
+    description=models.TextField()
+    image=models.ImageField(upload_to='/media')
+    priority=models.IntegerField(default=0)
