b3f902f97e4c3b4e6794ec2c4d9cdbc0ff11f9a3
itsbasil <itsbasilgeorge@gmail.com>
2024-07-12 09:09:19 +0530

    imported template.library to append data from db to product app in templatetags/

--- myenv/shop_project/products/views.py
+++ myenv/shop_project/products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from . models import Product
 
 # Create your views here.
 
@@ -6,7 +7,11 @@ def index(request):
     return render(request,'index.html')
 
 def list_products(request):
-    return render(request,'products.html')
+    product_list=Product.objects.all()
+    context={'products' : product_list}
+
+
+    return render(request,'products.html',context)
 
 def detail_product(request):
     return render(request,'product_detail.html')
