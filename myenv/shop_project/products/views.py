9bdea9dde7c9641a4e3e8f9249364eba668ef553
itsbasil <itsbasilgeorge@gmail.com>
2024-07-15 11:55:35 +0530

    added featured products & latest products to home page

--- myenv/shop_project/products/views.py
+++ myenv/shop_project/products/views.py
@@ -5,14 +5,19 @@ from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    featured_products=Product.objects.order_by('priority')[:4]
+    latest_products=Product.objects.order_by('-id')[:4]
+    context={'featured_products':featured_products,
+             'latest_products':latest_products}
+    print(context)
+    return render(request,'index.html',context)
 
 def list_products(request):
 
     page=1
     if request.GET:
         page=request.GET.get('page',1)
-    product_list=Product.objects.all()
+    product_list=Product.objects.order_by('-priority')
     product_paginator=Paginator(product_list,10)
     product_list=product_paginator.get_page(page)
     context={'products' : product_list}
