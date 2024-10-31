1afaaed9ddbae772b62312678da97c46e6c1cb05
itsbasil <itsbasilgeorge@gmail.com>
2024-10-31 23:04:41 +0530

    updated

--- myenv/shop_project/orders/models.py
+++ myenv/shop_project/orders/models.py
@@ -4,11 +4,13 @@ from products.models import Product
 
 # Create your models here.
 
+
+#data model for order
 class Order(models.Model):
     LIVE=1
     DELETE=0
     DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
-    CART_STAGE=1
+    CART_STAGE=0
     ORDER_CONFIRMED=1
     ORDER_PROCESSED=2
     ORDER_DELERED=3
@@ -23,6 +25,7 @@ class Order(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
+#model for ordered item
 class OrderedItem(models.Model):
     product=models.ForeignKey(Product,related_name='added_carts',on_delete=models.SET_NULL,null=True)
     quantity=models.IntegerField(default=1)
